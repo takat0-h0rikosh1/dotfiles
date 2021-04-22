d9b4deefbbd6565d3ada6a156d0ce412be84ae6a
takat0-h0rikosh1 <takato.h0rikosh1@gmail.com>
2021-04-22 11:18:51 +0900

    init hammerspoon

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,49 @@
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
+    hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
+end)
+
+-- Manual Reload
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
+  hs.reload()
+end)
+
+-- Auto Reload
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+
+-- Switch Input Source
+local simpleCmd = false
+local map = hs.keycodes.map
+local function eikanaEvent(event)
+    local c = event:getKeyCode()
+    local f = event:getFlags()
+    if event:getType() == hs.eventtap.event.types.keyDown then
+        if f['cmd'] then
+            simpleCmd = true
+        end
+    elseif event:getType() == hs.eventtap.event.types.flagsChanged then
+        if not f['cmd'] then
+            if simpleCmd == false then
+                if c == map['cmd'] then
+                    hs.keycodes.setMethod('Alphanumeric (Google)')
+                elseif c == map['rightcmd'] then
+                    hs.keycodes.setMethod('Hiragana (Google)')
+                end
+            end
+            simpleCmd = false
+        end
+    end
+end
+
+eikana = hs.eventtap.new({hs.eventtap.event.types.keyDown, hs.eventtap.event.types.flagsChanged}, eikanaEvent)
+eikana:start()
