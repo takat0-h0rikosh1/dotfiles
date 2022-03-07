eedd356eb8362df4a8497dd2ec729b22a091a17a
takat0-h0rikosh1 <takato.h0rikosh1@gmail.com>
2022-03-07 15:48:45 +0900

    update many

--- .zshrc
+++ .zshrc
@@ -10,3 +10,5 @@ fish
 #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
 export SDKMAN_DIR="/Users/takato.horikoshi/.sdkman"
 [[ -s "/Users/takato.horikoshi/.sdkman/bin/sdkman-init.sh" ]] && source "/Users/takato.horikoshi/.sdkman/bin/sdkman-init.sh"
+
+export PATH="$HOME/.poetry/bin:$PATH"
--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,11 @@
 # vi mode
 fish_vi_key_bindings
 
+# omf bobthefish
+set -g theme_powerline_fonts yes
+set -g theme_nerd_fonts yes
+set -g theme_color_scheme default
+
 # direnv
 eval (direnv hook fish)
 
@@ -17,14 +22,26 @@ alias gcm 'g cm'
 
 # jenv
 set -x PATH $HOME/.jenv/bin $PATH
+status --is-interactive; and source (jenv init -|psub)
 
 # pyenv
-if eval (command -v pyenv 1>/dev/null 2>&1)
-  pyenv init - | source
-end
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
+set -Ux PYENV_ROOT $HOME/.pyenv
+set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+pyenv init - | source
+set -x PATH $PYENV_ROOT/shims $PATH
+
+# poetry
+set -x PATH $HOME/.poetry/bin $PATH
 
 # stack
 # curl -sSL https://get.haskellstack.org/ | sudo sh
 set -x PATH $HOME/.local/bin $PATH
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/t_horikoshi/google-cloud-sdk/path.fish.inc' ]; . '/Users/t_horikoshi/google-cloud-sdk/path.fish.inc'; end
+
+# python3.8系だとgsutilが動かないので3.7を参照するワークアラウンド
+export CLOUDSDK_PYTHON=python3
+
+# nodebrew
+export PATH="$HOME/.nodebrew/current/bin:$PATH"
