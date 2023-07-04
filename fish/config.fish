cda5c394a7225819543a040b1e4f1b2c0d4ca9e8
Takato Horikoshi <takato.h0rikosh1@gmail.com>
2023-07-04 11:16:35 +0900

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -23,25 +23,3 @@ alias gcm 'g cm'
 # jenv
 set -x PATH $HOME/.jenv/bin $PATH
 status --is-interactive; and source (jenv init -|psub)
-
-# pyenv
-set -Ux PYENV_ROOT $HOME/.pyenv
-set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
-pyenv init - | source
-set -x PATH $PYENV_ROOT/shims $PATH
-
-# poetry
-set -x PATH $HOME/.poetry/bin $PATH
-
-# stack
-# curl -sSL https://get.haskellstack.org/ | sudo sh
-set -x PATH $HOME/.local/bin $PATH
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/t_horikoshi/google-cloud-sdk/path.fish.inc' ]; . '/Users/t_horikoshi/google-cloud-sdk/path.fish.inc'; end
-
-# python3.8系だとgsutilが動かないので3.7を参照するワークアラウンド
-export CLOUDSDK_PYTHON=python3
-
-# nodebrew
-export PATH="$HOME/.nodebrew/current/bin:$PATH"
